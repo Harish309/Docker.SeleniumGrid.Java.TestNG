950ab55fb386ada6b57adba1fdfaf31fc45b2d60
Harish Gajjela <gajjela43@gmail.com>
2020-10-11 23:43:15 +0530

    Updated POM XMl

--- src/test/java/docker/SetupDockerGrid.java
+++ src/test/java/docker/SetupDockerGrid.java
@@ -1,21 +1,30 @@
 package docker;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class SetupDockerGrid {
 	
-	@Test(priority=1)
-	public void StartDockerGrid() {
-		
+	@BeforeTest
+	public void StartDockerGrid() throws IOException, InterruptedException 
+	{
+		Runtime.getRuntime().exec("cmd /c start Start_DockerGrid.bat");
+		Thread.sleep(30000);//Need time for complete to start Docker
 	}
-	@Test(priority=1)
-	public void StopDockerGrid() {
+	
+	@AfterTest
+	public void StopDockerGrid() throws IOException, InterruptedException 
+	{
+		Runtime.getRuntime().exec("cmd /c start Stop_DockerGrid.bat");
+		Thread.sleep(30000);//Need time for complete to stop Docker
 		
+		Runtime.getRuntime().exec("taskkill /f /im cmd.exe"); //closes the cmd prompt window
 	}
-
 }
